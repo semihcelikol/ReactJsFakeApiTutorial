4293eac4752d1c405f57107175335173d12fdf77
Semih Çelikol <semihcelikol@outlook.com>
2021-11-03 00:51:59 +0300

    added is function component examples

--- src/component/todos.js
+++ src/component/todos.js
@@ -1,73 +1,54 @@
-import React, { Component } from 'react'
-import axios from "axios";
+import React, { useState, useEffect } from 'react'
 
-export default class Todos extends Component {
+const Todos = (props) => {
 
-    constructor(props){
-        super(props);
-
-        this.state = {
-            todoList: [],
-            isLoaded: false
-        }
+    const [todoList, setTodoList] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
+    
+    const getData = async () => {
+        
+        await fetch("https://jsonplaceholder.typicode.com/todos",{
+            method:"GET"
+        })
+        .then(response => response.json())
+        .then(data => {
+            setTodoList(data);
+            setIsLoaded(true);
+        })
+        .catch(error => alert(error));
     }
 
-    render() {
-        let ret;
+    useEffect(() => {
+        getData();
+    }, [])
 
-        if (this.state.isLoaded == true) {
-            ret= (
-                <div className="container">
-                    <table className="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">#</th>
-                                <th scope="col">User Id</th>
-                                <th scope="col">Title</th>
-                            </tr>
-                            </thead>
-                            <tbody>
-                                {
-                                    this.state.todoList.map(item => {
-                                        return (
-                                            <tr key={item.id}>
-                                                <th scope="row">{item.id}</th>
-                                                <td>{item.userId}</td>
-                                                <td>{item.title}</td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                    </table>
-                </div>
+    return (<div className="container">
+                <table className="table">
+                    <thead>
+                        <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">User Id</th>
+                            <th scope="col">Title</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                            {
+                                isLoaded &&
+                                
+                                todoList.map(item => {
+                                    return (
+                                        <tr key={item.id}>
+                                            <th scope="row">{item.id}</th>
+                                            <td>{item.userId}</td>
+                                            <td>{item.title}</td>
+                                        </tr>
+                                    )
+                                })
+                            }
+                        </tbody>
+                </table>
+            </div>
             )
-        }
-        else
-        {
-            ret = (
-                <div className="container">
-                    <h1>Error</h1>
-                </div>
-            )
-        }
-
-        return ret;
-    }
-
-    componentDidMount = async () => {
-
-        await axios.get("https://jsonplaceholder.typicode.com/todos")
-        .then(result => {
-            this.setState({
-                todoList: result.data,
-                isLoaded: true
-            });
-        }).catch(err => {
-            this.setState({
-                todoList: [],
-                isLoaded: false
-            });
-        });        
-    }
 }
+
+export default Todos
--- src/component/users.js
+++ src/component/users.js
@@ -1,83 +1,60 @@
-import React, { Component } from 'react'
-import axios from 'axios'
+import React, { useState, useEffect } from 'react'
 import {Link} from 'react-router-dom'
 import AddUser from './addUser';
 
-export default class Users extends Component {
+const Users = (props) => {
 
-    constructor(props){
-        super(props);
+    const [userList, setUserlist] = useState([]);
+    const [isLoaded, setIsLoaded] = useState(false);
 
-        this.state = {
-            userList: [],
-            isLoaded: false
-        }
+    const getData = () => {
+
+        fetch("https://jsonplaceholder.typicode.com/users",{
+            method:"GET"
+        })
+        .then(response => response.json())
+        .then(data => {
+            setUserlist(data);
+            setIsLoaded(true);
+        })
+        .catch(error => alert(error));
     }
 
-    render() {
-        let ret;
+    useEffect(() => {
+        getData()
+    },[])
 
-        if(this.state.isLoaded == true)
-        {
-            ret = (
-                <div className="container">
-                    
-                    <h1>User List</h1>
-                    <AddUser/>
-                    <hr></hr>
-                    <div className="row">
-                        { 
-                            this.state.userList.map(item=>{
-                                return(
-                                <div className="col-sm-6" key={item.id}>
-                                    <div className="card" style={{width:25 + 'rem'}} key={item.id}>
-                                        <div className="card-body">
-                                            <h5 className="card-title">{item.name}</h5>
-                                            <h6 className="card-subtitle mb-2 text-muted">Username: {item.username}</h6>
-                                            <p className="card-text"><b>Street: </b> {item.address.street}
-                                                                    <b> Suite: </b> {item.address.suite}
-                                                                    <b> City: </b> {item.address.city}
-                                                                    <b> Zipcode: </b> {item.address.zipcode}</p>
-                                            <p className="card-text"><b>Phone: </b>{item.phone}</p>
-                                            <p className="card-text"><b>Website: </b>{item.website}</p>
-                                            
-                                            <Link className="card-link"  to={`todos/${item.id}`}>Todos details</Link>
-                                            <Link className="card-link"  to={`posts/${item.id}`}>Posts details</Link>
-                                        </div>
-                                    </div>
+    return (
+        <div className="container">
+        <h1>User List</h1>
+        <AddUser/>
+        <hr></hr>
+            <div className="row">
+                { 
+                    userList.map(item=>{
+                        return(
+                        <div className="col-sm-6" key={item.id}>
+                            <div className="card" style={{width:25 + 'rem'}} key={item.id}>
+                                <div className="card-body">
+                                    <h5 className="card-title">{item.name}</h5>
+                                    <h6 className="card-subtitle mb-2 text-muted">Username: {item.username}</h6>
+                                    <p className="card-text"><b>Street: </b> {item.address.street}
+                                                            <b> Suite: </b> {item.address.suite}
+                                                            <b> City: </b> {item.address.city}
+                                                            <b> Zipcode: </b> {item.address.zipcode}</p>
+                                    <p className="card-text"><b>Phone: </b>{item.phone}</p>
+                                    <p className="card-text"><b>Website: </b>{item.website}</p>
+                                    
+                                    <Link className="card-link"  to={`todos/${item.id}`}>Todos details</Link>
+                                    <Link className="card-link"  to={`posts/${item.id}`}>Posts details</Link>
                                 </div>
-                                )
-                            })
-                        }
-                    </div>
-                </div>
-            )
-        }
-        else
-        {
-            ret = (
-                <div className="container">
-                    <h1>Error</h1>
-                </div>
-            )
-        }
-
-        return ret;
-    }
-
-    componentDidMount = async () => {
-        await axios.get("https://jsonplaceholder.typicode.com/users")
-        .then(result => {
-            this.setState({
-                userList: result.data,
-                isLoaded: true
-            });
-            
-        }).catch(err => {
-            this.setState({
-                userList: [],
-                isLoaded: false
-            });
-        });        
-    }
+                            </div>
+                        </div>
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
 }
+export default Users
